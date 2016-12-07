1bbad07cf26038b575a7c0cefee65fa53642ae32
David Baynard <davidbaynard@gmail.com>
2016-12-07 00:31:32 +0000

    Add forestay-serial

---
+++ forestay-serial/README.md
@@ -0,0 +1,9 @@
+# forestay-serial
+
+Want to do JSON, Yaml, protocol buffers and msgpack?
+
+This package meets your needs.
+
+Want to do one or two of them?
+
+Check out the source code!
---
+++ forestay-serial/src/Forestay/Serial.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE NoImplicitPrelude, OverloadedStrings, ApplicativeDo, PartialTypeSignatures #-}
+
+module Forestay.Serial (
+    module X
+  , MPObject
+  , ProtoVal
+  , encodeJSON
+  , decodeJSON
+  , encodeYaml
+  , decodeYaml
+  , encodeProtoBuf
+  , decodeProtoBuf
+)   where
+
+import Forestay.Data
+
+import qualified Data.Aeson as A
+import Data.Aeson as X hiding
+    ( encode
+    , decode
+    )
+
+import Data.Aeson.Lens as X
+
+import qualified Data.MessagePack as M
+import Data.MessagePack as X hiding
+    ( Object()
+    )
+
+import qualified Data.ProtocolBuffers as P
+import Data.ProtocolBuffers as X hiding
+    ( Value()
+    , encode
+    , decode
+    )
+
+import Data.ProtocolBuffers.Internal
+    ( Tag()
+    , WireField()
+    )
+
+import qualified Data.Yaml as Y
+import Data.Yaml as X hiding
+    ( encode
+    , decode
+    )
+
+import Data.HashMap.Strict
+import Data.Serialize
+
+type MPObject = M.Object
+
+type ProtoVal = P.Value
+
+encodeJSON :: ToJSON a => a -> LByteString
+encodeJSON = A.encode
+{-# INLINE encodeJSON #-}
+
+decodeJSON :: FromJSON a => LByteString -> Maybe a
+decodeJSON = A.decode
+{-# INLINE decodeJSON #-}
+
+encodeYaml :: ToJSON a => a -> ByteString
+encodeYaml = Y.encode
+{-# INLINE encodeYaml #-}
+
+decodeYaml :: FromJSON a => ByteString -> Maybe a
+decodeYaml = Y.decode
+{-# INLINE decodeYaml #-}
+
+encodeProtoBuf :: Encode a => a -> Put
+encodeProtoBuf = P.encode
+
+decodeProtoBuf :: Decode a => HashMap Tag [WireField] -> Get a
+decodeProtoBuf = P.decode
+
+
