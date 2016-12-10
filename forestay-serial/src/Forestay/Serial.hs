947fcf40cb5df245f76c7e2b4d26be4e46f53818
David Baynard <davidbaynard@gmail.com>
2016-12-10 16:58:22 +0000

    Upgrade serial exports

--- forestay-serial/src/Forestay/Serial.hs
+++ forestay-serial/src/Forestay/Serial.hs
@@ -2,8 +2,14 @@
 
 module Forestay.Serial (
     module X
-  , ProtoVal
   , AesonOptions
+  , CsvField
+  , CsvParser
+  , ProtoField
+  , ProtoVal
+  , YamlParser
+  , encodeCSV
+  , decodeCSV
   , encodeJSON
   , decodeJSON
   , encodeYaml
@@ -27,11 +33,22 @@ import Data.Aeson.Types as X hiding
 
 import Data.Aeson.Lens as X
 
+import qualified Data.Csv as C
+import Data.Csv as X hiding
+    ( encode
+    , decode
+    , Parser
+    , Field
+    , (.:)
+    , (.=)
+    )
+
 import qualified Data.ProtocolBuffers as P
 import Data.ProtocolBuffers as X hiding
     ( Value()
     , encode
     , decode
+    , Field
     )
 
 import Data.ProtocolBuffers.Internal
@@ -43,14 +60,31 @@ import qualified Data.Yaml as Y
 import Data.Yaml as X hiding
     ( encode
     , decode
+    , Parser
+    , (.:)
+    , (.=)
     )
 
 import Data.HashMap.Strict
 import Data.Serialize
 
-type ProtoVal = P.Value
-
 type AesonOptions = A.Options
+
+type CsvParser = C.Parser
+type CsvField = C.Field
+
+type ProtoVal = P.Value
+type ProtoField = P.Field
+
+type YamlParser = Y.Parser
+
+encodeCSV :: ToRecord a => [a] -> LByteString
+encodeCSV = C.encode
+{-# INLINE encodeCSV #-}
+
+decodeCSV :: FromRecord a => HasHeader -> LByteString -> Either String (Vector a)
+decodeCSV = C.decode
+{-# INLINE decodeCSV #-}
 
 encodeJSON :: ToJSON a => a -> LByteString
 encodeJSON = A.encode
