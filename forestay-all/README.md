a365b223153239f8603cd5b5a5583949a94a0a8c
David Baynard <davidbaynard@gmail.com>
2016-12-10 17:04:29 +0000

    Add forestay-all to check for clashes

---
+++ forestay-all/README.md
@@ -0,0 +1,5 @@
+# forestay-all
+
+Import and re-export all the forestay libraries.
+
+Mostly used to ensure no clashing exports.
---
+++ forestay-all/src/Forestay/All.hs
@@ -0,0 +1,8 @@
+{-# LANGUAGE NoImplicitPrelude, OverloadedStrings, ApplicativeDo, PartialTypeSignatures, TypeApplications #-}
+
+module Forestay.All (
+    module X
+)   where
+
+import Forestay as X
+import Forestay.Serial as X
