ae126ab6a206d7b7685efc12ab5e3608915b4213
David Baynard <davidbaynard@gmail.com>
2016-12-12 18:52:22 +0000

    Potentially improve efficiency of generic monoid derivation

--- forestay-data/src/Forestay/Deriving/Monoid.hs
+++ forestay-data/src/Forestay/Deriving/Monoid.hs
@@ -7,6 +7,9 @@ Maintainer  : David Baynard <davidbaynard@gmail.com>
 
 Based on Michael Snoyman’s Gist <https://gist.github.com/snoyberg/3769191>
 
+Splits the generic class into single-method classes, to avoid performance bug
+(see <https://github.com/kolmodin/binary/pull/95> for more information).
+
 -}
 
 module Forestay.Deriving.Monoid (
@@ -19,21 +22,26 @@ import GHC.Generics
 
 -- Generic version of Monoid. We'll need to create an instance for each of the
 -- Generic types.
-class GMonoid f where
+class GMempty f where
     gmempty :: f a
+
+class GMappend f where
     gmappend :: f a -> f a -> f a
 
 -- Nullary constructors. The instance is simple: mempty is the constructor,
 -- mappend is the constructor.
-instance GMonoid U1 where
+instance GMempty U1 where
     gmempty = U1
+
+instance GMappend U1 where
     gmappend U1 U1 = U1
 
 -- Products
-instance (GMonoid a, GMonoid b) => GMonoid (a :*: b) where
+instance (GMempty a, GMempty b) => GMempty (a :*: b) where
     -- Product of two gmempty values
     gmempty = gmempty :*: gmempty
 
+instance (GMappend a, GMappend b) => GMappend (a :*: b) where
     -- Apply gmappend recursively to the left and right and create a new
     -- product.
     gmappend (a :*: x) (b :*: y) = gmappend a b :*: gmappend x y
@@ -56,8 +64,10 @@ instance (GMonoid a, GMonoid b) => GMonoid (a :+: b) where
 -}
 
 -- Metadata: just a passthrough
-instance GMonoid a => GMonoid (M1 i c a) where
+instance GMempty a => GMempty (M1 i c a) where
     gmempty = M1 gmempty
+
+instance GMappend a => GMappend (M1 i c a) where
     gmappend (M1 x) (M1 y) = M1 $ gmappend x y
 
 -- Arguments: now use the real Monoid methods. We're essentially just
@@ -65,17 +75,19 @@ instance GMonoid a => GMonoid (M1 i c a) where
 --
 -- Note that this forces all of the fields in our datatype to be instances of
 -- Monoid, which is what we should expect.
-instance Monoid a => GMonoid (K1 i a) where
+instance Monoid a => GMempty (K1 i a) where
     gmempty = K1 mempty
+
+instance Monoid a => GMappend (K1 i a) where
     gmappend (K1 x) (K1 y) = K1 $ mappend x y
 
 -- Default implementations of mempty and mappend using gmempty and gmappend.
 -- All we do is use @to@ and @from@ to wrap and unwrap.
-defMempty :: (Generic a, GMonoid (Rep a)) => a
+defMempty :: (Generic a, GMempty (Rep a)) => a
 defMempty = to gmempty
 {-# INLINE defMempty #-}
 
-defMappend :: (Generic a, GMonoid (Rep a)) => a -> a -> a
+defMappend :: (Generic a, GMappend (Rep a)) => a -> a -> a
 defMappend x y = to $ from x `gmappend` from y
 {-# INLINE defMappend #-}
 
